31622c430ee72033ab8b116503fd2a94f20b5a03
Nemoto Mailon <minatokiroisen77@gmail.com>
2025-11-13 20:40:16 +0900

    upload the tech-blog app

---
+++ public/js/main.js
@@ -0,0 +1,43 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('/posts')
+        .then((response) => response.json())
+        .then((posts) => {
+            const postContainer = document.getElementById('posts')
+            postContainer.innerHTML = posts.map((post) => `
+                <div class="post ${post.category}">
+                    <h1>${post.title}</h1>
+                    ${
+                        post.image
+                        ? `<img class="post-img" src="${post.image}" alt="${post.title}" />`
+                        : ""
+                    }
+                    <h3 class="post-category">${post.category}</h3>
+                    <a href="/post-details/${encodeURIComponent(
+                            post.title.trim().replace(/\s+/g, "-").toLowerCase()
+                        )}">
+                        <h2 class="post-title">${post.title}</h2>
+                    </a>
+                    <div class="post-desc">${post.content}</div>
+                    <a href="/post-details/${encodeURIComponent(
+                            post.title.trim().replace(/\s+/g, "-").toLowerCase()
+                        )}" class="read-more">
+                        Read More
+                    </a>
+                </div>
+            `).join("")
+        })
+        .catch((error) => {
+            console.error("Error fetching posts:", error)
+            const postContainer = document.getElementById("posts")
+            postContainer.innerHTML = "<p>Error Fetching Posts.</p>"
+        })
+})
+
+// HEADER
+
+let header = document.querySelector("header")
+
+window.addEventListener("scroll", () => {
+    header.classList.toggle("shadow", window.scrollY > 0)
+})
